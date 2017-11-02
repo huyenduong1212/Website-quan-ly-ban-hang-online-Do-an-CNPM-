8be99d80c150fdcbd8763b76ad7a9f047e7312f0
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2017-11-02 17:23:28 +0700

    add all files

---
+++ WebSiteBanHang/Controllers/KhachHangController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+
+
+namespace WebSiteBanHang.Controllers
+{
+    [Authorize(Roles = "QuanTri,QuanLySANPHAM")]
+    public class KHACHHANGController : Controller
+    {
+        //
+        // GET: /KHACHHANG/
+       QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        [Authorize(Roles = "QuanLySANPHAM")]
+        //public ActionResult Index()
+        //{
+        //    //Truy vấn dữ liệu thông qua câu lệnh
+        //    //Đối lstKH sẽ lấy toàn bộ dữ liệu từ bản khách hàng
+        //    //Cách 1: Lấy dữ liệu là 1 danh sách khách hàng
+        //    //var lstKH = from KH in db.KHACHHANGs select KH;
+        //    //Cách 2: Dùng phương thức hổ trợ sẵn
+        //    //var lstKH = db.KHACHHANGs;
+        //    //return View(lstKH);
+
+        //}
+                 [Authorize(Roles = "QuanTri")]
+        //public ActionResult Index1()
+        //{
+        //    //var lstKH = from KH in db.KHACHHANGs select KH;
+        //    return View(lstKH);
+        //}
+        //public ActionResult TruyVan1DoiTuong()
+        //{
+        //    //Cách 1: Truy vấn 1 đối tượng bằng câu lệnh truy vấn
+        //    //Bước1: Lấy ra danh sách khách hàng 
+        //    var lstKH = from kh in db.KHACHHANGs where kh.MaKH==2 select kh ;
+        //    //Buoc2: 
+        //    //KHACHHANG khang = lstKH.FirstOrDefault();
+        //    //Lấy đối tượng khách hàng dựa trên phương thức hổ trợ
+        //    KHACHHANG khang = db.KHACHHANGs.SingleOrDefault(n=>n.MaKH==2);
+        //    return View(khang);
+        //}
+        //public ActionResult SortDuLieu()
+        //{ 
+        //    //Phương thức sắp xếp dữ liệu
+        //    List<KHACHHANG> lstKH = db.KHACHHANGs.OrderByDescending(n => n.TenKH).ToList();
+        //    return View(lstKH);
+                 
+        //}
+        public ActionResult GroupDuLieu()
+        {
+            //Group dữ liệu
+            List<NGUOIDUNG> lstKH = db.NGUOIDUNGs.OrderByDescending(n => n.TaiKhoan).ToList();
+            return View(lstKH);
+        }
+       
+	}
+}
---
+++ WebSiteBanHang/Controllers/ThongKeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.ApplicationServices;
+using WebSiteBanHang.Models;
+
+
+namespace WebSiteBanHang.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        //
+        // GET: /ThongKe/
+        [HttpGet]
+        public ActionResult ThongKe()
+        {
+            //
+            ViewBag.PageView = HttpContext.Application["PageView"].ToString();
+            ViewBag.Online = HttpContext.Application["Online"].ToString();
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetJsonData(int Year=2015)
+        {
+            var lstDonDatHang = db.DONDATHANGs.Where(n=>n.ThoiDiemDat.Value.Year==Year).ToList();
+            var list = new List<ThongKeDoanhThu>();
+           
+            if(lstDonDatHang.Count>0)
+            {
+                ThongKeDoanhThu tk;
+                for (int i = 1; i <= 12; i++)
+                {
+                    tk = new ThongKeDoanhThu();
+                    tk.Nam = Year;
+                    tk.Thang = i;
+                    var lstDoanhThu = lstDonDatHang.Where(n => n.ThoiDiemDat.Value.Month == i);
+                    if (lstDonDatHang.Count() > 0)
+                    {
+                        tk.DoanhThu = 0;
+                        foreach(var item in lstDoanhThu)
+                        {
+                            //tk.DoanhThu += item.CHITIETDONDATHANGs.Sum(n => n.SoLuong * n.DonGia).Value;
+                     
+                            tk.DoanhThu = 10000;
+                        }
+                    }
+                    list.Add(tk);
+                }
+             }
+
+         
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                    db.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+	}
+}
